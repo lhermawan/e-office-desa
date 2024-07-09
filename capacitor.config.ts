f84f07c00271fbc3a88609c0dc9362cce42a92bd
Lucky Hermawan Roza <luckyhermawanroza@gmail.com>
2024-07-09 02:08:15 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'E-Office Desa',
+  webDir: 'www'
+};
+
+export default config;
